e2f22830a1c4f049b687ee88a7d04e3991cb4a68
julieg18 <juliannagal18@gmail.com>
2020-06-22 12:14:46 -0500

    refactor: refactor index.js to improve readability

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -4,48 +4,21 @@ import PopupWithImage from '../components/PopupWithImage.js';
 import PopupWithForm from '../components/PopupWithForm.js';
 import UserInfo from '../components/UserInfo.js';
 import Section from '../components/Section.js';
+import {
+  profileAddButton,
+  profileEditButton,
+  forms,
+  initialCards,
+  settingsObject,
+} from '../utils/constants.js';
 
-const profileEditButton = document.querySelector('.profile__button_type_edit');
-const profileAddButton = document.querySelector('.profile__button_type_add');
-const forms = Array.from(document.querySelectorAll('.form'));
-const imagePopup = new PopupWithImage('.popup_content_picture');
 const profileInfo = new UserInfo({
   nameSelector: '.profile__name',
   jobSelector: '.profile__job',
 });
-const initialCards = [
-  {
-    text: 'Lake Louise',
-    imgLink: './images/lake-louise.jpg',
-  },
-  {
-    text: 'Bald Mountains',
-    imgLink: './images/bald-mountains.jpg',
-  },
-  {
-    text: 'Latemar',
-    imgLink: './images/latemar.jpg',
-  },
-  {
-    text: 'Vanois National...',
-    imgLink: './images/vanois-national.jpg',
-  },
-  {
-    text: 'Lago di Braies',
-    imgLink: './images/lago-di-braies.jpg',
-  },
-  {
-    text: 'Yosemite Valley',
-    imgLink: './images/yosemite-valley.jpg',
-  },
-];
-const settingsObject = {
-  inputSelector: '.form__field',
-  submitButtonSelector: '.form__submit-button',
-  inactiveButtonClass: 'form__submit-button_inactive',
-  inputErrorClass: 'form__field_type_error',
-  errorClass: 'form__field-error_active',
-};
+
+const imagePopup = new PopupWithImage('.popup_content_picture');
+imagePopup.setEventListeners();
 
 function handleCardClick(data) {
   imagePopup.open(data);
@@ -71,10 +44,6 @@ const cards = new Section(
 );
 cards.renderItems();
 
-function editFormSubmitHandler({ 'name-field': name, 'job-field': job }) {
-  profileInfo.setUserInfo({ name, job });
-}
-
 function addCardFormSubmitHandler({
   'title-field': text,
   'img-link-field': imgLink,
@@ -82,23 +51,25 @@ function addCardFormSubmitHandler({
   cards.addItem(createCard({ text, imgLink }));
 }
 
-forms.forEach((form) => {
-  const newFormValidator = new FormValidator(settingsObject, form);
-  newFormValidator.enableValidation();
-});
-
 const addCardFormPopup = new PopupWithForm(
   addCardFormSubmitHandler,
   '.popup_content_add-card-form',
 );
+addCardFormPopup.setEventListeners();
+
+function editFormSubmitHandler({ 'name-field': name, 'job-field': job }) {
+  profileInfo.setUserInfo({ name, job });
+}
 
 const editProfileFormPopup = new PopupWithForm(
   editFormSubmitHandler,
   '.popup_content_edit-profile-form',
 );
-
-imagePopup.setEventListeners();
-addCardFormPopup.setEventListeners();
 editProfileFormPopup.setEventListeners();
+
+forms.forEach((form) => {
+  const newFormValidator = new FormValidator(settingsObject, form);
+  newFormValidator.enableValidation();
+});
 profileEditButton.addEventListener('click', () => editProfileFormPopup.open());
 profileAddButton.addEventListener('click', () => addCardFormPopup.open());
